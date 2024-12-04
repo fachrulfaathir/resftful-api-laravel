eae5a292b15157e64e43f3e8b304d2e467eb102f
Fachrul Faathirullah <fachrulfaathir36@gmail.com>
2024-12-04 16:20:17 +0700

    pos insert dan get details

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+//posts
+Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+
